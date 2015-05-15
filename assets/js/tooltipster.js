b5e909528e4beccd1048258705416177fea45d7f
Kartikkeyan KM <kmkartik@gmail.com>
2015-05-15 17:05:41 +0530

    Changed modal z-index and added skill content

--- assets/js/tooltipster.js
+++ assets/js/tooltipster.js
@@ -12,16 +12,16 @@
 				content: $('<div class="skill"><h4>Adobe InDesign</h4><ul><li>Print Designs</li><li>Interactive Documents</li><li>Typography designs</li></ul></div>')
             });
 			$('#flash').tooltipster({
-				content: $('<div class="skill"><h4>Adobe Flash</h4><ul><li>Tween animations</li><li>Basic web interface animations</li><li>Actionscript 3.0</li></ul></div>')
+				content: $('<div class="skill"><h4>Adobe Flash</h4><ul><li>Product based animations</li><li>Basic web interface animations</li><li>Actionscript 3.0</li><li>2d Pre production animations</li></ul></div>')
             });
 			$('#dreamweaver').tooltipster({
 				content: $('<div class="skill"><h4>Adobe Dreamweaver</h4><ul><li>Old Tabular web layout</li><li>PSD to HTML conversion</li><li>Responsive Design</li></ul></div>')
             });
 			$('#premierepro').tooltipster({
-				content: $('<div class="skill"><h4>Adobe Premiere Pro</h4><ul><li>Video editing</li><li>Adding Effects, Transitions and Titling</li></ul></div>')
+				content: $('<div class="skill"><h4>Adobe Premiere Pro</h4><ul><li>Video and audio editing</li><li>Adding Effects, Transitions and Titling</li><li>Color Correction</li></ul></div>')
             });
 			$('#aftereffects').tooltipster({
-				content: $('<div class="skill"><h4>After effects</h4><ul><li>Green/Bluescreen Keying, Masking, and Rotobrush</li><li>Compositing and animations</li><li>Usage of few external pluginss</li></ul></div>')
+				content: $('<div class="skill"><h4>After effects</h4><ul><li>Green/Bluescreen Keying, Masking, and Rotobrush</li><li>Compositing and animations</li><li>Usage of few external plugins</li></ul></div>')
             });
 			$('#mochapro').tooltipster({
 				content: $('<div class="skill"><h4>Mocha Pro</h4><ul><li>Basic interface</li><li>Rotoscoping for After effects and Nuke</li><li>Track data for after effects</li></ul></div>')
